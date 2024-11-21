c24ce4a3aa9524c456c5d64805713fe46a3c494e
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2024-11-21 16:25:00 -0500

    Modal dismiss should happen on background only

--- js/quickref.js
+++ js/quickref.js
@@ -67,8 +67,8 @@ function init() {
     fill_section(data_environment_vision, "environment-vision", "Environment");
     fill_section(data_environment_cover, "environment-cover", "Environment");
 
-    var modal = document.getElementById("modal");
-    modal.onclick = hide_modal;
+    var modal = document.getElementByClassName("modal-backdrop");
+    modal[0].onclick = hide_modal;
 }
 
 $(window).load(init);
